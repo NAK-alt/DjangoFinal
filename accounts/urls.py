1dd772257ca74359709ce8c024ea480dbded415f
Dyy <dyy93268557@gmail.com>
2025-12-05 10:20:16 +0700

    first commit

---
+++ accounts/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('no/', views.home),
+    path('products/', views.products),
+    path('customer/', views.customer),
+
+
+    path('', views.index, name='index'),
+    path('blank/', views.blank, name='blank'),
+    path('product/', views.product, name='product'),
+    path('store/', views.store, name='store'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('HotDeal/', views.HotDeal ,name='HotDeal'),
+    path('Categories/', views.Categories ,name='Categories'),
+    path('product/', views.product ,name='product'),
+    path('laptop/', views.laptop ,name='laptop'),
+    path('cameras/', views.cameras ,name='cameras'),
+    path('smartphone/', views.smartphone ,name='smartphone'),
+    path('accessories/', views.accessories ,name='accessories'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,49 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse('Home_page')
+
+def products(request):
+    return HttpResponse('Products_page')
+
+def customer(request):
+    return HttpResponse('Customer_page')
+
+def index(request):
+    return render(request,'electro/index.html')
+
+def blank(request):
+    return render(request,'electro/blank.html')
+
+def product(request):
+    return render(request,'electro/product.html')
+
+def store(request):
+    return render(request,'electro/store.html')
+
+def checkout(request):
+    return render(request,'electro/checkout.html')
+
+def HotDeal(request):
+    return render(request,'electro/HotDeal.html')
+
+def Categories(request):
+    return render(request,'electro/categories.html')
+
+def product(request):
+    return render(request,'electro/product.html')
+
+def laptop(request):
+    return render(request,'electro/laptop.html')
+
+def cameras(request):
+    return render(request,'electro/cameras.html')
+
+def smartphone(request):
+    return render(request,'electro/smartphone.html')
+
+def accessories(request):
+    return render(request,'electro/accessories.html')
