e7d6a463df85e1ab7c2dc979f46728f1faff0c0a
NAK <phalvongvathanak007@gmail.com>
2025-12-25 20:58:52 +0700

    stress nas

--- crm/urls.py
+++ crm/urls.py
@@ -1,24 +1,28 @@
-"""
-URL configuration for crm project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+# Import views from your app (replace 'accounts' with your app name if different)
+from accounts import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # App URLs
     path('', include('accounts.urls')),
-    path("ckeditor/", include("ckeditor_uploader.urls")),
+
+    # CKEditor
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+
+    # Product and Cart URLs
+    path('product/<int:pk>/', views.productDetail, name='productDetail'),
+    path('cart/', views.view_cart, name='view_cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<int:product_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('checkout/', views.checkout_view, name='checkout_view'),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
